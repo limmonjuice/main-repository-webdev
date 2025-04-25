620f2794ddcf86adb90f396f9f286b3e82d1b501
limmonjuice <zaimond.lim@lorma.edu>
2025-04-26 00:18:10 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const lenders = document.getElementById("nakautangs");
+const errorMsg = document.getElementById("errorMsg");
+
+document.getElementById("add").addEventListener("click", function() {
+    let name = document.getElementById("utanger").value;
+    let amount = parseFloat(document.getElementById("money").value);
+
+    if (name == "" || isNaN(amount)) {
+        errorMsg.textContent = "Please enter valid inputs.";
+        errorMsg.classList.add("error");
+        return;
+    }
+
+    errorMsg.classList.remove("error");
+    errorMsg.textContent = "";
+
+    const newUtang = document.createElement("li");
+    newUtang.textContent = name + " - ₱" + amount;
+    lenders.appendChild(newUtang);
+
+    document.getElementById("utanger").value = "";
+    document.getElementById("money").value = "";
+});
+
+document.getElementById("clear").addEventListener("click", function() {
+    lenders.textContent = "";
+});
